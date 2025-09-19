f8788cf2dd40d6fea7d05ef7aa227bff9945e241
Boris Vilboux <boris.vilboux@edf-re.fr>
2025-09-19 23:01:25 +0200

    install routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from '@tanstack/react-router'
-import { router } from './routes/__root.tsx'
+import { router } from './routes/router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -16,6 +16,9 @@ const rootRoute = createRootRoute({
         </Link>{' '}
         <Link to="/about" className="[&.active]:font-bold">
           About
+        </Link>{' '}
+        <Link to="/info" className="[&.active]:font-bold">
+          Info
         </Link>
       </div>
       <hr />
@@ -45,7 +48,38 @@ const aboutRoute = createRoute({
   },
 })
 
-const routeTree = rootRoute.addChildren([indexRoute, aboutRoute])
+const infoRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/info',
+  component: function Info() {
+    return (
+      <div className="p-2">
+        <h3>Info Page</h3>
+        <div className="flex gap-2 mt-2">
+          <Link to="/info/details" className="[&.active]:font-bold text-blue-500">
+            Details
+          </Link>
+        </div>
+        <hr className="my-2" />
+        <Outlet />
+      </div>
+    )
+  },
+})
+
+const infoDetailsRoute = createRoute({
+  getParentRoute: () => infoRoute,
+  path: '/details',
+  component: function InfoDetails() {
+    return <div className="p-2 bg-gray-100">This is the details section inside Info!</div>
+  },
+})
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  aboutRoute,
+  infoRoute.addChildren([infoDetailsRoute])
+])
 
 export const router = createRouter({ routeTree })
 
