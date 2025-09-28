6a261b04a735491131a76a3ad6e96eb9081d4e43
Boris Vilboux <boris.vilboux@edf-re.fr>
2025-09-28 18:43:36 +0200

    scroolbar for mobile

--- src/components/mobile/App-mobile.tsx
+++ src/components/mobile/App-mobile.tsx
@@ -1,23 +1,26 @@
-import { Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import {
-  Box,
-} from '@mui/material'
 
+import { Outlet } from '@tanstack/react-router';
+import { BorderLayout } from '../../share/component/layout/BorderLayout';
 import { AppBanner } from './AppBanner';
 import { AppCommand } from './AppCommand';
+import { Box } from '@mui/material';
 
 export function AppMobile() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <AppBanner/> 
-      <Box sx={{ flex: 1, display: 'flex'}}>
-          <Outlet />
-      </Box>
+    <BorderLayout sx={{ height: '100%' }}
+      north={<AppBanner />}
+      center={<Box sx={{ overflow: 'auto', height: '100%' }}><Outlet/></Box>}
+      south={<AppCommand />}
+    />
+    // <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+    //   <AppBanner/> 
+    //   <Box sx={{ flex: 1, display: 'flex'}}>
+    //       <Outlet />
+    //   </Box>
 
-      <AppCommand></AppCommand>
+    //   <AppCommand></AppCommand>
 
-      <TanStackRouterDevtools />
-    </Box>
+    //   <TanStackRouterDevtools />
+    // </Box>
   )
 }
