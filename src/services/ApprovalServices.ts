bbe6f40fde0ea75cf7825ee2c09515be75845f52
Boris Vilboux <boris.vilboux@edf-re.fr>
2025-11-16 12:52:59 +0100

    adding approval services

---
+++ src/services/ApprovalServices.ts
@@ -0,0 +1,11 @@
+import type { Survey } from "../types/survey.types";
+
+export async function getLatestSurveyData(): Promise<Survey> {
+  const proxyUrl = 'https://corsproxy.io/?';
+  const jsonUrl = 'https://github.com/MieuxVoter/mj-tracker-2027/releases/download/latest-data/latest_survey_approval_compact.json';
+  const jsonResponse = await fetch(proxyUrl + encodeURIComponent(jsonUrl));
+  if (!jsonResponse.ok) {
+    throw new Error(`Erreur lors du chargement des données: ${jsonResponse.status}`);
+  }
+  return await jsonResponse.json();
+}
